ed201ec0b75b873e37b79e6b433b8b74f08e63dd
Martin Smola <smola.martin@gmail.com>
2017-03-30 15:32:01 +0200

    Updated function component

--- componentmaker/function.js
+++ componentmaker/function.js
@@ -15,20 +15,23 @@ exports.options = { code: `
 // leave bellow code as is unless you use async operation
 // in that case assign the data using following "flowdata.data=\'my new data\'"
 // and call next(value); from within callback function of your async operation
+// see info tab on the right for more details
 next(value);
 ` };
 
 exports.readme = `# Function
 Allows you to do sync operation on data
 
+WARNING: read warning bellow examples
+
 __Custom function__:
 
 \`\`\`javascript
-next;     // function to call once done
-value;    //
-instance; // ref to value.instance, available methods get, set, rem for storing data related to this instance of Function component
-global;   // ref to value.global, available methods get, set, rem for storing data globally accessible in any component
-flowdata; // ref to value.flowdata, instance of FlowData - available methods get, set, rem for storing data related to current flow
+next;    	// function to call once done
+value;    	//
+instance; 	// ref to value.instance, available methods get, set, rem for storing temporary data related to this instance of Function component
+global;   	// ref to value.global, available methods get, set, rem for storing persistent data globally accessible in any component
+flowdata; 	// ref to value.flowdata, instance of FlowData - available methods get, set, rem for storing temporary data related to current flow
 
 // Example #1:
 flowdata.data = 'Hello world.';
@@ -39,10 +42,19 @@ db.insert(data).callback(function(err){
 	flowdata.data = { err: err };
 	next(value);
 })
+
+// WARNING!! below code will not update flowdata.data property
+// flowdata.data === 'incomming data';
+var data = flowdata.data;
+data = 'hello world';
+next(value);
+// in the next component the flowdata.data will still be 'incomming data'
+// always use
+flowdata.data = 'hello world';
 \`\`\``;
 
 exports.html = `<div class="padding">
-	<div data-jc="codemirror" data-jc-path="code" data-required="true">@(Code)</div>
+	<div data-jc="codemirror" data-type="javascript" data-jc-path="code" data-required="true" data-height="500px">@(Code)</div>
 </div>`;
 
 exports.install = function(instance) {
@@ -61,19 +73,21 @@ exports.install = function(instance) {
 			get: FLOW.get,
 			set: FLOW.set,
 			rem: FLOW.rem
-		}
+		},
+		Date: Date
 	};
 
 	instance.custom.reconfigure = function(){
-		// fn = new Function('instance', 'flowdata', 'next', instance.options.code);
-		fn = SCRIPT('var instance = value.instance;var flowdata = value.flowdata;' + instance.options.code);
+		// fn = new Function('value', instance.options.code);
+		fn = SCRIPT('var instance = value.instance;var flowdata = value.flowdata;var Date = value.Date;var global = value.global;' + instance.options.code);
 	};
 
 	instance.on('data', function(flowdata) {
 		VALUE.flowdata = flowdata;
+
 		fn(VALUE, function(err, value) {
 			if (err)
-				instance.error('Error while processing function');
+				instance.error('Error while processing function ' + err);
 			else
 				instance.send(value.flowdata);
 		});
