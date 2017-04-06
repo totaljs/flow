b2a551c061218c7e26e9e4997ff613e443b5c8e8
Peter Širka <petersirka@gmail.com>
2017-04-06 11:01:31 +0200

    Improve readme.

--- componentmaker/messengerusers.js
+++ componentmaker/messengerusers.js
@@ -10,7 +10,30 @@ exports.readme = `# Total.js Messenger: Users
 
 This component evaluates user data when the user is online (green output) / offline (red output).
 
-- output contains a raw data of the user`;
+- output contains a raw data of the user
+
+\`\`\`javascript
+{
+	id: 'IDENTIFICATOR'
+	name: 'Peter Širka',
+	online: true,
+	datecreated: Date,
+	datelogged: Date,
+	position: 'Web Developer',
+	department: 'IT department',
+	picture: 'IDENTIFICATOR', // needs to be formated like this /photos/{ID}.jpg
+	linker: 'peter-sirka',
+	email: 'petersirka@gmail.com',
+	status: 'My work is my hobby',
+	notifications: true,
+	unreadcount: 3, // how many messages is unread?
+	unread: {}, // key === ID
+	channels: {}, // key === ID
+	lastmessages: {}, // key === ID of user or channel
+	mobile: false, // is from mobile device?
+	sa: true // is super admin?
+}
+\`\`\``;
 
 exports.install = function(instance) {
 
