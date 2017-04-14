48d96469b80fb0a55b886e202296dd97cfbd4ac0
Martin Smola <smola.martin@gmail.com>
2017-04-14 22:11:47 +0200

    Updated Function component

--- componentmaker/function.js
+++ componentmaker/function.js
@@ -7,79 +7,108 @@ exports.input = true;
 exports.output = 1;
 exports.version = '1.0.0';
 exports.author = 'Martin Smola';
-exports.options = { code: `
-
-
-
-
-// leave bellow code as is unless you use async operation
-// in that case assign the data using following "flowdata.data=\'my new data\'"
-// and call next(value); from within callback function of your async operation
-// see info tab on the right for more details
-next(value);
-` };
+exports.options = { 
+	outputs: 1,
+	code: `send('Hello world!');`};
 
 exports.readme = `# Function
 Allows you to do sync operation on data
 
-WARNING: read warning bellow examples
+If \`send\` function isn't called the data flow will not continue.
 
 __Custom function__:
 
 \`\`\`javascript
-next;    	// function to call once done
-value;    	//
-instance; 	// ref to value.instance, available methods get, set, rem for storing temporary data related to this instance of Function component
+value;    	// 
+send;    	// send data to next component, optionaly specify output index -> send(0, data);
+instance; 	// ref to value.instance, available methods get, set, rem for storing temporary data related to this instance of Function component and  debug, status and error for sending data to designer
 global;   	// ref to value.global, available methods get, set, rem for storing persistent data globally accessible in any component
 flowdata; 	// ref to value.flowdata, instance of FlowData - available methods get, set, rem for storing temporary data related to current flow
+Date;		// 
 
 // Example #1:
-flowdata.data = 'Hello world.';
-next(value);
-
-// Example #2:
-db.insert(data).callback(function(err){
-	flowdata.data = { err: err };
-	next(value);
-})
-
-// WARNING!! below code will not update flowdata.data property
-// flowdata.data === 'incomming data';
-var data = flowdata.data;
-data = 'hello world';
-next(value);
-// in the next component the flowdata.data will still be 'incomming data'
-// always use
-flowdata.data = 'hello world';
+send('Hello world.'); // sends data to all outputs
+send(0, 'Hello world.'); // sends data only to first output
 \`\`\``;
 
 exports.html = `<div class="padding">
+	<div class="row">
+		<div class="col-md-3">
+			<div data-jc="textbox" data-jc-type="number" data-jc-path="outputs" data-validate="value > 0" data-increment="true" data-maxlength="3">Number of outputs</div>
+			<div class="help m">@(Minimum is 1)</div>
+		</div>
+	</div>
 	<div data-jc="codemirror" data-type="javascript" data-jc-path="code" data-required="true" data-height="500px">@(Code)</div>
-</div>`;
+</div>
+<script>
+	var function_outputs_count;
+
+	ON('open.function', function(component, options) {
+		function_outputs_count = options.outputs = options.outputs || 1;
+	});
+	
+	ON('save.function', function(component, options) {
+		if (function_outputs_count !== options.outputs) {
+			component.connections = {};
+			component.output = options.outputs || 1;
+			setState(MESSAGES.apply);
+		}
+	});
+</script>`;
 
 exports.install = function(instance) {
 
 	var fn;
 	var VALUE = {
 		instance: {
-			get: instance.get,
-			set: instance.set,
-			rem: instance.rem,
-			error: instance.error,
-			debug: instance.debug,
-			status: instance.status
+			get: instance.get.bind(instance),
+			set: instance.set.bind(instance),
+			rem: instance.rem.bind(instance),
+			error: instance.error.bind(instance),
+			debug: instance.debug.bind(instance),
+			status: instance.status.bind(instance),
+			send: function(flowdata, index, data){
+
+				if (data instanceof Array) {
+					for (let i = 0, length = data.length; i < length; i++){
+						flowdata.data = data[i];
+						instance.send(i, flowdata);
+					}
+				} else {
+					flowdata.data = data;
+					instance.send(index, flowdata);
+				}
+			}
 		},
 		global: {
 			get: FLOW.get,
 			set: FLOW.set,
 			rem: FLOW.rem
 		},
-		Date: Date
+		Date: Date,
+		Object: Object
 	};
 
 	instance.custom.reconfigure = function(){
 		// fn = new Function('value', instance.options.code);
-		fn = SCRIPT('var instance = value.instance;var flowdata = value.flowdata;var Date = value.Date;var global = value.global;' + instance.options.code);
+		fn = SCRIPT(`
+			var instance = value.instance;
+			var flowdata = value.flowdata;
+			var Date = value.Date;
+			var Object = value.Object;
+			var global = value.global;
+			var send = function(index, data){
+				if (!data){
+					data = index;
+					index = 0;
+				} 
+				value.instance.send(value.flowdata, index || undefined, data);
+			}
+
+			${instance.options.code}
+
+			next(value);
+		`);
 	};
 
 	instance.on('data', function(flowdata) {
@@ -87,9 +116,9 @@ exports.install = function(instance) {
 
 		fn(VALUE, function(err, value) {
 			if (err)
-				instance.error('Error while processing function ' + err);
-			else
-				instance.send(value.flowdata);
+				return instance.error('Error while processing function ' + err);
+
+			//instance.send(value.flowdata);
 		});
 	});
 
