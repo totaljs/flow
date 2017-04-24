e34f86ba0acbc0419193af5c304fa7d0351ab95f
Martin Smola <smola.martin@gmail.com>
2017-04-24 18:49:06 +0200

    Added Comment component

---
+++ componentmaker/comment.js
@@ -0,0 +1,11 @@
+exports.id = 'comment';
+exports.title = 'Comment';
+exports.group = 'Common';
+exports.color = '#704cff';
+exports.author = 'Martin Smola';
+exports.icon = '';
+exports.readme = `# Comment`;
+
+exports.install = function(instance) {
+
+};
