4f1991c600003324f7c70a88a343fd433c3204b4
molda <smola.martin@gmail.com>
2017-04-27 19:20:22 +0200

    Updated httproute

--- componentmaker/httproute.js
+++ componentmaker/httproute.js
@@ -19,11 +19,17 @@ When a request comes in bellow object is available at \`flowdata.data\`:
 }
 \`\`\`
 
-If the Label field in the settings form is left empty then following string will be used automatically: METHOD /some/url`;
+See documentation for flags [here](https://docs.totaljs.com/latest/en.html#api~HttpRouteOptionsFlags~unauthorize)
+Method flags are set automatically e.g. \`get, post, put or delete\`
+
+\`id:ROUTE_ID\` flag cannot be used since it's already used by this component internally
+`;
 
 exports.html = `<div class="padding">
 	<div data-jc="textbox" data-jc-path="url" class="m" data-required="true" data-maxlength="500" data-placeholder="/api/test">@(URL address)</div>
-	<div data-jc="dropdown" data-jc-path="method" data-required="true" data-options=";GET;POST;PUT;DELETE" class="m">@(HTTP method)</div>
+	<div data-jc="dropdown" data-jc-path="method" data-required="true" data-options=";GET;POST;PUT;DELETE;OPTIONS" class="m">@(HTTP method)</div>
+	<div data-jc="textbox" data-jc-path="flags" data-placeholder="json">@(Additional flags)</div>
+	<div class="help m">@(Separate flags by comma e.g. 'json,authorize')</div>
 	<div data-jc="checkbox" data-jc-path="emptyresponse">@(Automaticlly respond with 200 OK?)</div>
 	<div class="help m">@(If not checked you need to use HTTP response component to respond to the request.)</div>
 	<div data-jc="keyvalue" data-jc-path="headers" data-placeholder-key="@(Header name)" data-placeholder-value="@(Header value and press enter)" class="m">@(Custom headers)</div>
@@ -70,7 +76,7 @@ exports.install = function(instance) {
 		}
 
 		if (typeof(options.flags) === 'string')
-			options.flags = options.flags.split('|');
+			options.flags = options.flags.split(',');
 
 		id && UNINSTALL('route', id);
 		id = 'id:' + instance.id;
