573da219cb1e030622ab7325ef4a4a7fda9383bb
Peter Širka <petersirka@gmail.com>
2017-05-25 17:32:22 +0200

    Add new UI components.

--- source/ui.js
+++ source/ui.js
@@ -3169,3 +3169,74 @@ COMPONENT('nosqlcounter', function() {
 		self.html(builder);
 	};
 });
+
+COMPONENT('colorpicker', function() {
+	var self = this;
+	var container;
+	var template = '<li data-index="{0}"><i class="fa fa-circle" style="color:#{1}"></i></li>';
+	var opener = {};
+	var is = false;
+	var colors = 'ff0000,ff4000,ff8000,ffbf00,ffff00,bfff00,80ff00,40ff00,00ff00,00ff40,00ff80,00ffbf,00ffff,00bfff,0080ff,0040ff,0000ff,4000ff,8000ff,bf00ff,ff00ff,ff00bf,ff0080,ff0040,ff0000,F0F0F0,D0D0D0,AAB2BD,505050,000000'.split(',');
+
+	self.singleton();
+	self.readonly();
+	self.blind();
+
+	self.make = function() {
+		self.classes('ui-colorpicker hidden');
+		self.append('<ul></ul>');
+		container = self.find('ul');
+
+		self.event('click', 'li', function() {
+			var color = '#' + colors[+this.getAttribute('data-index')];
+			opener.callback && opener.callback(color, opener.target);
+			self.hide();
+			EMIT('colorpicker', color);
+		});
+
+		self.on('reflow', self.$hide);
+		self.on('resize', self.$hide);
+	};
+
+	self.$hide = function() {
+		is && self.hide();
+	};
+
+	self.render = function() {
+		var builder = [];
+		for (var i = 0, length = colors.length; i < length; i++)
+			builder.push(template.format(i, colors[i]));
+		container.empty().html(builder.join(''));
+	};
+
+	self.hide = function() {
+		if (!is)
+			return;
+		self.classes('hidden');
+		is = false;
+	};
+
+	self.show = function(x, y, target, callback) {
+
+		if (is && opener.x === x && opener.y === y) {
+			opener.x = null;
+			opener.y = null;
+			self.hide();
+			return;
+		}
+
+		if (typeof(target) === 'function') {
+			callback = target;
+			target = null;
+		}
+
+		opener.callback = callback;
+		opener.target = target;
+		opener.x = x;
+		opener.y = y;
+
+		!is && self.render();
+		is = true;
+		self.element.css({ left: x, top: y }).removeClass('hidden');
+	};
+});
