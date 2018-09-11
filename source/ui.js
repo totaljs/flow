17a8549fffc857d4f145d78c66b0f014a7a370f1
Peter Širka <petersirka@gmail.com>
2018-09-11 10:48:51 +0200

    Fixed ui.js

--- source/ui.js
+++ source/ui.js
@@ -1082,7 +1082,7 @@ COMPONENT('websocket', 'reconnect:2000', function(self, config) {
 
 		sending = true;
 		var async = queue.splice(0, 3);
-		async.waitFor(function(item, next) {
+		async.wait(function(item, next) {
 			ws.send(item);
 			setTimeout(next, 5);
 		}, function() {
