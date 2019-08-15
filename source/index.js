b53911f87d157bb152d1292abe51ca29b1be39f6
Peter Širka <petersirka@gmail.com>
2019-08-15 09:10:37 +0200

    Updated version.

--- source/index.js
+++ source/index.js
@@ -37,7 +37,7 @@ var READY = false;
 var MODIFIED = null;
 var TYPE;
 
-exports.version = 'v6.1.1';
+exports.version = 'v6.1.2';
 
 global.FLOW = { components: {}, instances: {}, inmemory: {}, triggers: {}, alltraffic: { count: 0 }, indexer: 0, loaded: false, url: '', $events: {}, $variables: '', variables: EMPTYOBJECT, outputs: {}, inputs: {} };
 global.FLOW.version = +exports.version.replace(/[v.]/g, '');
