870e793820c43a7bba3b94a29b0d42e16e044303
Peter Sirka <petersirka@gmail.com>
2020-11-15 00:04:44 +0100

    Updated code.

--- source/ui.js
+++ source/ui.js
@@ -1187,7 +1187,7 @@ COMPONENT('validation', 'delay:100;flags:visible', function(self, config) {
 	};
 });
 
-COMPONENT('websocket', 'reconnect:2000', function(self, config) {
+COMPONENT('websocket', 'reconnect:3000;encoder:true', function(self, config) {
 
 	var ws, url;
 	var queue = [];
@@ -1195,41 +1195,53 @@ COMPONENT('websocket', 'reconnect:2000', function(self, config) {
 
 	self.online = false;
 	self.readonly();
-	self.nocompile();
+	self.nocompile && self.nocompile();
 
 	self.make = function() {
-		url = config.url || '';
+		url = (config.url || '').env(true);
 		if (!url.match(/^(ws|wss):\/\//))
 			url = (location.protocol.length === 6 ? 'wss' : 'ws') + '://' + location.host + (url.substring(0, 1) !== '/' ? '/' : '') + url;
 		setTimeout(self.connect, 500);
 		self.destroy = self.close;
+
+		$(W).on('offline', function() {
+			self.close();
+		});
+
+		$(W).on('online', function() {
+			setTimeout(self.connect, config.reconnect);
+		});
+
 	};
 
 	self.send = function(obj) {
-		queue.push(JSON.stringify(obj));
+		var data = JSON.stringify(obj);
+		if (config.encoder)
+			queue.push(encodeURIComponent(data));
+		else
+			queue.push(data);
 		self.process();
 		return self;
 	};
 
-	self.configure = function(key, value, init) {
-		if (init)
-			return;
-		if (key === 'url')
-			url = value;
-	};
-
 	self.process = function(callback) {
 
-		if (!ws || sending || !queue.length || ws.readyState !== 1) {
+		if (!ws || !ws.send || sending || !queue.length || ws.readyState !== 1) {
 			callback && callback();
 			return;
 		}
 
 		sending = true;
 		var async = queue.splice(0, 3);
+
 		async.wait(function(item, next) {
-			ws.send(item);
-			setTimeout(next, 5);
+			if (ws) {
+				ws.send(item);
+				setTimeout(next, 5);
+			} else {
+				queue.unshift(item);
+				next();
+			}
 		}, function() {
 			callback && callback();
 			sending = false;
@@ -1244,56 +1256,56 @@ COMPONENT('websocket', 'reconnect:2000', function(self, config) {
 		ws.onopen = ws.onclose = ws.onmessage = null;
 		!isClosed && ws.close();
 		ws = null;
-		EMIT('online', false);
+		self.isonline(false);
 		return self;
+	};
+
+	self.isonline = function(is) {
+		if (config.online)
+			self.EXEC(config.online, is);
+		else
+			EMIT('online', is);
 	};
 
 	function onClose(e) {
 
 		if (e.code === 4001) {
-			location.reload(true);
+			location.href = location.href + '';
 			return;
 		}
 
-		var r = e.reason;
+		e.reason && WARN('WebSocket:', config.encoder ? decodeURIComponent(e.reason) : e.reason);
 		self.close(true);
-		if (r) {
-
-			if (r.indexOf('request length') !== -1)
-				SETTER('message', 'warning', '<i class="fa fa-warning mr5"></i><b>UNEXPECTED ERROR</b><div class="mt10">Flow wasn\'t saved due to the exceed designer size. You need to increase the limit.</div>');
-
-			if (r.indexOf('401') !== -1) {
-				setTimeout(function() {
-					location.reload(true);
-				}, 2000);
-			}
-		}
-
 		setTimeout(self.connect, config.reconnect);
 	}
 
 	function onMessage(e) {
+
 		var data;
+
 		try {
-			data = PARSE(e.data);
-			self.attrd('jc-path') && self.set(data);
+			data = PARSE(config.encoder ? decodeURIComponent(e.data) : e.data);
 		} catch (e) {
-			WARN('WebSocket "{0}": {1}'.format(url, e.toString()));
+			return;
 		}
-		data && EMIT('message', data);
+
+		if (config.message)
+			self.EXEC(config.message, data);
+		else
+			EMIT('message', data);
 	}
 
 	function onOpen() {
 		self.online = true;
 		self.process(function() {
-			EMIT('online', true);
+			self.isonline(true);
 		});
 	}
 
 	self.connect = function() {
 		ws && self.close();
-		setTimeout2(self.id, function() {
-			ws = new WebSocket(url);
+		setTimeout2(self.ID, function() {
+			ws = new WebSocket(url.env(true));
 			ws.onopen = onOpen;
 			ws.onclose = onClose;
 			ws.onmessage = onMessage;
