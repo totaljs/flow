4fa58f114e7455e11d28b0e1cf243d5e1092dbdc
Peter Širka <petersirka@gmail.com>
2021-11-12 11:17:31 +0100

    Fixed validation in the Settings.

--- source/ui.js
+++ source/ui.js
@@ -1155,20 +1155,19 @@ COMPONENT('visible', function(self, config) {
 	};
 });
 
-COMPONENT('validation', 'delay:100;flags:visible', function(self, config) {
+COMPONENT('validation', 'delay:100;flags:visible', function(self, config, cls) {
 
-	var path, elements = null;
+	var elements = null;
 	var def = 'button[name="submit"]';
 	var flags = null;
+	var tracked = false;
+	var reset = 0;
+	var old, track;
 
 	self.readonly();
 
 	self.make = function() {
 		elements = self.find(config.selector || def);
-		path = self.path.replace(/\.\*$/, '');
-		setTimeout(function() {
-			self.watch(self.path, self.state, true);
-		}, 50);
 	};
 
 	self.configure = function(key, value, init) {
@@ -1185,17 +1184,60 @@ COMPONENT('validation', 'delay:100;flags:visible', function(self, config) {
 				} else
 					flags = null;
 				break;
+			case 'track':
+				track = value.split(',').trim();
+				break;
 		}
 	};
 
-	self.state = function() {
-		setTimeout2(self.id, function() {
-			var disabled = DISABLED(path, flags);
-			if (!disabled && config.if)
-				disabled = !EVALUATE(self.path, config.if);
-			elements.prop('disabled', disabled);
-		}, config.delay);
+	var settracked = function() {
+		tracked = 0;
 	};
+
+	self.setter = function(value, path, type) {
+
+		var is = path === self.path || path.length < self.path.length;
+
+		if (reset !== is) {
+			reset = is;
+			self.tclass(cls + '-modified', !reset);
+		}
+
+		if ((type === 1 || type === 2) && track && track.length) {
+			for (var i = 0; i < track.length; i++) {
+				if (path.indexOf(track[i]) !== -1) {
+					tracked = 1;
+					return;
+				}
+			}
+			if (tracked === 1) {
+				tracked = 2;
+				setTimeout(settracked, config.delay * 3);
+			}
+		}
+	};
+
+	var check = function() {
+		var path = self.makepath(self.path).replace(/\.\*$/, '');
+		var disabled = tracked || config.validonly ? !VALID(path, flags) : DISABLED(path, flags);
+		if (!disabled && config.if)
+			disabled = !EVALUATE(path, config.if);
+		if (disabled !== old) {
+			elements.prop('disabled', disabled);
+			self.tclass(cls + '-ok', !disabled);
+			self.tclass(cls + '-no', disabled);
+			old = disabled;
+		}
+	};
+
+	self.state = function(type, what) {
+		if (type === 3 || what === 3) {
+			self.rclass(cls + '-modified');
+			tracked = 0;
+		}
+		setTimeout2(self.ID, check, config.delay);
+	};
+
 });
 
 COMPONENT('websocket', 'reconnect:3000;encoder:true', function(self, config) {
