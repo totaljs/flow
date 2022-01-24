d181d8e1b129f33984799d6195b79558122a91c7
Peter Širka <petersirka@gmail.com>
2022-01-24 07:25:06 +0100

    Fixed protection.

--- definitions/auth.js
+++ definitions/auth.js
@@ -1,18 +1,20 @@
+var BLACKLIST = {};
+
 AUTH(function($) {
+
+	if (BLACKLIST[$.ip] > 15) {
+		$.invalid();
+		return;
+	}
 
 	var token = $.cookie(CONF.cookie);
 	if (token) {
-
-		if (BLOCKED($, 15)) {
-			$.invalid();
-			return;
-		}
-
 		var session = DECRYPTREQ($.req, token, CONF.cookie_secret);
 		if (session && session.id === PREF.user.id && session.expire > NOW) {
 			$.success({ sa: true });
 			return;
-		}
+		} else
+			BLACKLIST[$.ip] = (BLACKLIST[$.ip] || 0) + 1;
 	}
 
 	$.invalid();
@@ -22,3 +24,8 @@ if (!PREF.user) {
 	var password = GUID(10);
 	PREF.set('user', { id: UID(), login: GUID(10), password: password.sha256(CONF.cookie_secret), raw: password });
 }
+
+ON('service', function(counter) {
+	if (counter % 15 === 0)
+		BLACKLIST = {};
+});
