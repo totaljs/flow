fbbf2de5dbf28bf90d2b642c362c118d36428a96
Peter Širka <petersirka@gmail.com>
2022-05-13 14:48:55 +0200

    Fixed memory management.

--- modules/flowstream.js
+++ modules/flowstream.js
@@ -1097,10 +1097,10 @@ function init_worker(meta, type, callback) {
 
 	var forkargs = [F.directory, '--fork'];
 
-	if (CONF.flowstream_memory)
+	if (meta.memory)
 		forkargs.push('--max-old-space-size=' + meta.memory);
 
-	var worker = type === true || type === 'worker' ? (new W.Worker(__filename, { workerData: meta })) : Fork(__filename, forkargs, { serialization: 'json' }); // detached: true,
+	var worker = type === true || type === 'worker' ? (new W.Worker(__filename, { workerData: meta })) : Fork(__filename, forkargs, { serialization: 'json' });
 	var ischild = false;
 
 	meta.unixsocket = F.isWindows ? ('\\\\?\\pipe\\flowstream' + F.directory.makeid() + meta.id + Date.now().toString(36)) : (F.Path.join(F.OS.tmpdir(), 'flowstream_' + F.directory.makeid() + '_' + meta.id + '_' + Date.now().toString(36) + '.socket'));
